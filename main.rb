2911c3fc46c081fe91b2664b43688cefde5745d4
euqueme <euqueme@gmail.com>
2019-10-28 16:01:23 -0600

    MY_ANY
    Passes some dummy test cases and also  had to disable complexity metrics

--- main.rb
+++ main.rb
@@ -38,5 +38,22 @@ module Enumerable
     output
   end
 
+  def my_any?(arg = nil)
+    output = false
+    my_each do |element|
+      if output == false
+        if block_given?
+          output = yield(element)
+        elsif arg
+          output = element.is_a?(arg) unless arg.is_a?(Regexp)
+          output = element.match?(arg) if arg.is_a?(Regexp)
+        else
+          output = true unless element
+        end
+      end
+    end
+    output
+  end
+
   # rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
 end
